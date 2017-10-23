29865be133bd310c6439189045e660b0ec9e8c17
Daniel Park <dpeace926@gmail.com>
2017-10-23 14:34:54 -0400

    install react-router, create top ten components

--- top-ten-react/src/App.js
+++ top-ten-react/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1.js'
+import Page2 from './components/Page2.js'
+import Page3 from './components/Page3.js'
+import Page4 from './components/Page4.js'
+import Page5 from './components/Page5.js'
+import Page6 from './components/Page6.js'
+import Page7 from './components/Page7.js'
+import Page8 from './components/Page8.js'
+import Page9 from './components/Page9.js'
+import Page10 from './components/Page10.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <Redirect to='/page1' />
+        <Route path="/page1" component={Page1}/>
+        <Route path="/page2" component={Page2}/>
+        <Route path="/page3" component={Page3}/>
+        <Route path="/page4" component={Page4}/>
+        <Route path="/page5" component={Page5}/>
+        <Route path="/page6" component={Page6}/>
+        <Route path="/page7" component={Page7}/>
+        <Route path="/page8" component={Page8}/>
+        <Route path="/page9" component={Page9}/>
+        <Route path="/page10" component={Page10}/>
       </div>
     );
   }
---
+++ top-ten-react/src/components/Page1.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { Route, Redirect, Link } from 'react-router-dom'
+
+class Page1 extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  render = () => {
+    return (
+      <div>
+        [page1]
+      </div>
+    )
+  }
+}
+
+export default Page1
